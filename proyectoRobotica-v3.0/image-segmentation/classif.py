749a7bb39cc91a50ccad77767581e70c9e1ff303
Alejandro-Cobo <alejandro.cobo.cabornero@alumnso.upm.es>
2019-03-14 12:35:08 +0100

    Update classif.py & prac_run.py

--- proyectoRobotica-v3.0/image-segmentation/classif.py
+++ proyectoRobotica-v3.0/image-segmentation/classif.py
@@ -1,51 +1,12 @@
 import numpy as np
-from scipy.spatial import distance
 
-class Classifier:
-    def __init__(self):
-        pass
-
-    def predict(self):
-        pass
-
-    def segmenta(self):
-        pass
-
-
-class segEuclid(Classifier):
+class segEuclid():
     def __init__(self, data):
-        self.labels = np.array(range(len(data)))
-        self.Z = np.zeros(shape=(len(self.labels),np.size(data[0],axis=1)))
-        for i in range(len(self.labels)):
-            self.Z[i] = np.mean(data[i], axis=0)
-        pass
-
-    def predict(self, X):
-        if X.shape[2] != self.Z.shape[1]:
-            Z = ((self.Z+0.0).T/np.sum(self.Z,axis=1)).T[:,:2]
-        else:
-            Z = self.Z
-        return X.dot(Z.T) - 0.5*np.sum(np.power(Z,2), axis=1)
+        self.Z = np.zeros((3,2))
+        means = np.zeros((3,3))
+        for i in range(len(data)):
+            means[i] = np.mean(data[i],0)
+        self.Z = ((means).T/np.sum(means,1)).T[:,:2]
 
     def segmenta(self, X):
-        return self.labels[np.argmax(self.predict(X), axis=2)]
-
-
-class segMano(Classifier):
-    def __init__(self, data):
-        self.labels = np.array(range(len(data)))
-        self.Z = np.zeros(shape=(len(self.labels),3))
-        self.invCov = np.zeros(shape=(len(self.labels),3,3))
-        for i in range(len(self.labels)):
-            self.invCov[:,:,i] = np.linalg.inv( np.cov(data[i], rowvar=False, ddof=1) )
-            self.Z[i] = np.mean(data[i], axis=0)
-        pass
-
-    def predict(self, X):
-        R = np.zeros(X.shape)
-        for i in range(len(self.labels)):
-            R[:,:,i] = np.power(X-self.Z[i],2).dot(self.invCov[i])
-        return R
-            
-    def segmenta(self, X):
-        return self.labels[np.argmin(self.predict(X), axis=2)]
+        return np.argmax(X.dot(self.Z.T)-0.5*np.sum(np.power(self.Z, 2), 1), 2)
--- proyectoRobotica-v3.0/image-segmentation/prac_run.py
+++ proyectoRobotica-v3.0/image-segmentation/prac_run.py
@@ -12,7 +12,6 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 import classif as seg
-
 import time
 
 # Leo las imagenes de entrenamiento
@@ -24,27 +23,28 @@ data_marca=imNp[np.where(np.all(np.equal(markImg,(255,0,0)),2))]
 data_fondo=imNp[np.where(np.all(np.equal(markImg,(0,255,0)),2))]
 data_linea=imNp[np.where(np.all(np.equal(markImg,(0,0,255)),2))]
 
+
 # Creo y entreno los segmentadores euclideos
 start = time.time()
 segmEuc = seg.segEuclid([data_fondo, data_linea, data_marca])
-# segmMano = seg.segMano([data_fondo, data_linea, data_marca])
 end = time.time()
 print("Tiempo de entrenamiento: " + str(end-start))
+
 # Inicio la captura de imagenes
 capture = cv2.VideoCapture("video1.mp4")
+
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
 out = cv2.VideoWriter('video_segmentado.avi', fourcc, 100/25, (320,240))
 
 # Ahora clasifico el video
-key = 0
 count = 0
 im_count = 1
-while key != ord('q'):
+while True:
     # voy a segmentar solo una de cada 25 imagenes y la muestro
-    key = cv2.waitKey(1)
     ret, img = capture.read()
+    cv2.waitKey(1)
     
-    if count%25 != 0:
+    if count%5 != 0:
         count += 1
         continue
         
@@ -61,10 +61,10 @@ while key != ord('q'):
     # Compute rgb normalization
     if count == 0:
         start = time.time()
+        
     imrgbn = np.rollaxis((np.rollaxis(imNp,2)+0.0)/np.sum(imNp,2),0,3)[:,:,:2]
-
     labelsEu=segmEuc.segmenta(imrgbn)
-    # labelsMa=segmMano.segmenta(imNp)
+
     if count == 0:
         end = time.time()
         print("Tiempo de segmentacion: " +str(end-start))
