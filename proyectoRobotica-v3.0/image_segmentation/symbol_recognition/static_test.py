b3eb57b9051754cdeb47e7cb671232a07d053fd9
Alejandro-Cobo <alejandro.cobo.cabornero@alumnso.upm.es>
2019-05-01 22:45:26 +0200

    Update static test

--- proyectoRobotica-v3.0/image_segmentation/symbol_recognition/static_test.py
+++ proyectoRobotica-v3.0/image_segmentation/symbol_recognition/static_test.py
@@ -9,22 +9,22 @@ data, labels = hu.get_hu_moments()
 
 # 1-NN
 loo = cv.LeaveOneOut(len(labels))
-errors = []
+errors = 0.0
 for tr, te in loo:
     knn = sklearn.neighbors.KNeighborsClassifier(1)
     knn = knn.fit(data[tr],labels[tr])
     ypred = knn.predict(data[te])
-    errors.append((sum(ypred==labels[te])+0.0)/len(labels[te]))
+    errors += ypred != labels[te]
 print("k-NN:")
-print("\t- Porcentaje de acierto: " + str(sum(errors)/len(errors)*100) + "%")
+print("\t-- Porcentaje de acierto: " + str((1-(errors/len(labels)))*100) + "%")
 
 # Mahalanobis
 loo = cv.LeaveOneOut(len(labels))
-errors = []
+errors = 0.0
 for tr, te in loo:
     maha = mahalanobis.classifMahalanobis()
     maha = maha.fit(data[tr],labels[tr])
     ypred = maha.predict(data[te])
-    errors.append((sum(ypred==labels[te])+0.0)/len(labels[te]))
+    errors += ypred != labels[te]
 print("Mahalanobis:")
-print("\t- Porcentaje de acierto: " + str(sum(errors)/len(errors)*100) + "%")
+print("\t-- Porcentaje de acierto: " + str((1-(errors/len(labels)))*100) + "%")
